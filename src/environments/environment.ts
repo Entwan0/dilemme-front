7d77a4b8d1d75f03f4c11daed7adba3debb8cdf3
volozan <volozan.antoine@gmail.com>
2021-11-18 14:07:54 +0100

    deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
   api: { url: 'https://intense-escarpment-15728.herokuapp.com/' },
+  production: true,
 };
 
 /*
