5e1275451e8df6bc0a3cf35cd04fcd43a1d7f10b
volozan <volozan.antoine@gmail.com>
2021-11-18 14:19:12 +0100

    add imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { TuiButtonModule, TuiRootModule } from '@taiga-ui/core';
 import { AppComponent } from './app.component';
 import {TuiTagModule} from "@taiga-ui/kit";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     TuiButtonModule,
     TuiRootModule,
